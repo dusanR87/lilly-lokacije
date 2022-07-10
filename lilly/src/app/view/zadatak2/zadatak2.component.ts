0a336582f7a670e9aa2844da0e063a0429d302ec
DESKTOP-RDS2NFU\Korisnik <dusan.rajcic87@gmail.com>
2022-07-10 12:47:52 +0200

    dorade

--- lilly/src/app/view/zadatak2/zadatak2.component.ts
+++ lilly/src/app/view/zadatak2/zadatak2.component.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { testService } from 'src/app/services/test.services';
 
 import { Product, ProductMapped } from 'src/app/models/brands-models';
+import { switchMap, map, filter } from 'rxjs';
 
 
 @Component({
@@ -11,11 +12,11 @@ import { Product, ProductMapped } from 'src/app/models/brands-models';
   styleUrls: ['./zadatak2.component.scss']
 })
 export class Zadatak2Component implements OnInit {
-  kesiraniPodaci: string|undefined;
+  kesiraniPodaci: any;
 
   public prozivod1: Observable<Product[]> = of([{id: 1, Name : 'proizvod 1'},{id: 2, Name : 'proizvod 2'},{id: 3, Name : 'proizvod 3'},{id: 4, Name : 'proizvod 4'}])
   public prozivodMapirani1: Observable<ProductMapped[]> = of([]) 
-
+    public map2: Observable<any[]> = of([])
   public prozivodMapirani?: ProductMapped[]
 
   constructor(private service: testService) { }
@@ -25,22 +26,32 @@ export class Zadatak2Component implements OnInit {
 
   kesiraj() {
     this.onClickClear()
-    this.service.getAuthToken().subscribe(x=>{
+    /*this.service.getAuthToken().subscribe(x=>{
       this.service.getPromotionsWithAuthorization(x['accessToken']).subscribe(x=>{
         console.log('podaci dobijeni rucnim dodavanjem accessToken-a',x)
         this.kesiraniPodaci = x;
       })
-    })
+    })*/
+    this.service.getAuthToken().pipe(
+      switchMap((token: any) => {
+         return this.service.getPromotionsWithAuthorization(token['accessToken']);
+      })
+     ).subscribe(x => {
+      console.log('podaci dobijeni rucnim dodavanjem accessToken-a',x)
+      this.kesiraniPodaci = x
+    });
+    
   }
 
   onClickClear() {
     this.kesiraniPodaci = undefined;
     this.prozivodMapirani = undefined;
+    this.prozivodMapirani1 = of([])
   }
 
   mapirajPodatke() {
     this.onClickClear()
-    this.prozivod1.subscribe((data)=>{
+    /* this.prozivod1.subscribe((data)=>{
       data.map(y=> {
         this.prozivodMapirani1.subscribe(n=>n.push({id:y['id'],Name:y['Name'],Code:y['id'].toString()}))
       }
@@ -50,7 +61,14 @@ export class Zadatak2Component implements OnInit {
     this.prozivodMapirani1.subscribe(x=>{
       console.log('mapirani',x)
       this.prozivodMapirani = x
-    })
+    }) */
+    
+    this.prozivodMapirani1 = this.prozivod1.pipe(
+      map(x=>x.map(
+        ({id,Name})=>({id:id,Name:Name,Code:id.toString()})
+      )
+    ))
+    this.prozivodMapirani1.subscribe(x=>this.prozivodMapirani = x)
   }
 
 }
